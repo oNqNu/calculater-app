ac07e09d1037ecb0b147e3ab5c123c4b83808b18
oNqNu <onanukintaro@gmail.com>
2025-02-09 00:07:23 +0900

    Add Terser plugin for JavaScript code optimization and update README with build optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    terser({
+      format: {
+        comments: false,
+      },
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
+      },
+      mangle: {
+        properties: {
+          regex: /^_/,  // _で始まるプロパティ名のみマングリング
+        },
+      },
+    }),
+  ],
   base: '/calculater-app/',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    sourcemap: false,  // ソースマップを無効化
+  },
 })
